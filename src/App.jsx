207c108363f7a8598172815f4126dbe460721680
edwin141999 <gendevilman@gmail.com>
2023-05-08 20:54:43 -0600

    react-victory

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Balance from "./components/Balance";
+import ExpenseChart from "./components/ExpenseChart";
 import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionForm from "./components/transactions/TransactionForm";
 import TransactionList from "./components/transactions/TransactionList";
@@ -8,7 +9,7 @@ export default function App() {
   return (
     <GlobalProvider>
       <div className="bg-zinc-950 text-white h-screen flex justify-center items-center">
-        <div className="container mx-auto w-2/6">
+        <div className="container mx-auto w-3/6">
           <div className="bg-zinc-800 p-10 rounded-lg flex gap-x-2">
             <div>
               <h1 className="text-4xl font-bold">Expense Tracker </h1>
@@ -16,7 +17,8 @@ export default function App() {
               <Balance />
               <TransactionForm />
             </div>
-            <div className="w-full">
+            <div className="flex flex-col flex-1">
+              <ExpenseChart />
               <TransactionList />
             </div>
           </div>
--- src/components/transactions/TransactionForm.jsx
+++ src/components/transactions/TransactionForm.jsx
@@ -3,8 +3,8 @@ import { useGlobalState } from "../../context/GlobalState";
 
 export default function TransactionForm() {
   const { addTransaction } = useGlobalState();
-  const [description, setDescription] = useState([]);
-  const [amount, setAmount] = useState([]);
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState(0);
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -13,6 +13,8 @@ export default function TransactionForm() {
       description,
       amount: +amount,
     });
+    setAmount(0);
+    setDescription("");
   };
 
   return (
@@ -23,6 +25,7 @@ export default function TransactionForm() {
           placeholder="Enter a Description"
           onChange={(e) => setDescription(e.target.value)}
           className="bg-zinc-600 text-white px-3 py-2 rounded-lg w-full block mb-2"
+          value={description}
         />
         <input
           type="Number"
@@ -30,6 +33,7 @@ export default function TransactionForm() {
           step={"0.01"}
           onChange={(e) => setAmount(e.target.value)}
           className="bg-zinc-600 text-white px-3 py-2 rounded-lg w-full block mb-2"
+          value={amount}
         />
         <button className="bg-indigo-700 text-white px-3 py-2 rounded-lg block mb-2 w-full">
           Add Transaction
